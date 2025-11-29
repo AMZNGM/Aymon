b97de95a5538e1a56d7c78e4882ba55c4c2c5f34
NGM <ppmm91453@gmail.com>
2025-11-29 20:27:01 +0200

    step one

---
+++ src/data/media-data/media-imports.js
@@ -0,0 +1,74 @@
+import mainPhoto from '../../../public/images/mainPhoto.webp'
+import asfour from '../../../public/images/randomImgs/asfour.webp'
+import aymonPortrait from '../../../public/images/randomImgs/aymonPortrait.webp'
+import caligula from '../../../public/images/randomImgs/caligula.webp'
+import crow from '../../../public/images/randomImgs/crow.webp'
+import folk from '../../../public/images/randomImgs/folk.webp'
+import forcing from '../../../public/images/randomImgs/forcing.webp'
+import haramt from '../../../public/images/randomImgs/haramt.webp'
+import inside from '../../../public/images/randomImgs/inside.webp'
+import metro from '../../../public/images/randomImgs/metro.webp'
+import perspective from '../../../public/images/randomImgs/perspective.webp'
+import pigeon from '../../../public/images/randomImgs/pigeon.webp'
+import proof from '../../../public/images/randomImgs/proof.webp'
+import working from '../../../public/images/randomImgs/working.webp'
+import blitz1 from '../../../public/images/selectedImgs/blitz/blitz1.webp'
+import blitz2 from '../../../public/images/selectedImgs/blitz/blitz2.webp'
+import blitz3 from '../../../public/images/selectedImgs/blitz/blitz3.webp'
+import blitz4 from '../../../public/images/selectedImgs/blitz/blitz4.webp'
+import blitz5 from '../../../public/images/selectedImgs/blitz/blitz5.webp'
+import blitz6 from '../../../public/images/selectedImgs/blitz/blitz6.webp'
+import blitz7 from '../../../public/images/selectedImgs/blitz/blitz7.webp'
+import geo1 from '../../../public/images/selectedImgs/geo/geo1.webp'
+import geo2 from '../../../public/images/selectedImgs/geo/geo2.webp'
+import geo3 from '../../../public/images/selectedImgs/geo/geo3.webp'
+import geo4 from '../../../public/images/selectedImgs/geo/geo4.webp'
+import geo5 from '../../../public/images/selectedImgs/geo/geo5.webp'
+import Menage from '../../../public/images/selectedImgs/ghadaAbdelrazikXMénage07/Menage.webp'
+import halal from '../../../public/images/selectedImgs/marwanPablo/halal.webp'
+
+const awarnessUrbnlanes = '../../../public/videos/randomVideos/awarnessUrbnlanes.mp4'
+const coldInside = '../../../public/videos/randomVideos/coldInside.mp4'
+const eyes = '../../../public/videos/randomVideos/eyes.mp4'
+const feeling = '../../../public/videos/randomVideos/feeling.mp4'
+const plastine = '../../../public/videos/randomVideos/plastine.mp4'
+const visualCard = '../../../public/videos/randomVideos/visualCard.mp4'
+const marwanPablo = '../../../public/videos/selectedVideos/marwanPablo.mp4'
+
+export {
+  mainPhoto,
+  asfour,
+  aymonPortrait,
+  caligula,
+  crow,
+  folk,
+  forcing,
+  haramt,
+  inside,
+  metro,
+  perspective,
+  pigeon,
+  proof,
+  working,
+  blitz1,
+  blitz2,
+  blitz3,
+  blitz4,
+  blitz5,
+  blitz6,
+  blitz7,
+  geo1,
+  geo2,
+  geo3,
+  geo4,
+  geo5,
+  Menage,
+  halal,
+  awarnessUrbnlanes,
+  coldInside,
+  eyes,
+  feeling,
+  plastine,
+  visualCard,
+  marwanPablo,
+}
