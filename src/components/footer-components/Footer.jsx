8495e7bf13a55d1de5bf0bf7840caf4e138c98c8
NGM <ppmm91453@gmail.com>
2025-12-10 14:34:01 +0200

    footer color

--- src/components/footer-components/Footer.jsx
+++ src/components/footer-components/Footer.jsx
@@ -2,7 +2,7 @@ import FooterContent from '@/components/footer-components/FooterContent'
 
 export default function Footer() {
   return (
-    <footer className="relative w-screen h-[800px] bg-main/50" style={{ clipPath: 'inset(0% 0% 0% 0%)' }}>
+    <footer className="relative w-screen h-[800px] bg-bg/50" style={{ clipPath: 'inset(0% 0% 0% 0%)' }}>
       <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
         <div className="sticky top-[calc(100vh-800px)] h-[800px]">
           <FooterContent />
