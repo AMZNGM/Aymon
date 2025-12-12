0709be8b8564ba09da705bce3c117e531febd4c0
NGM <ppmm91453@gmail.com>
2025-12-12 22:16:09 +0200

    remove the text from the videSection

--- src/components/VideosSection.jsx
+++ src/components/VideosSection.jsx
@@ -1,8 +1,3 @@
-'use client'
-
-import { motion } from 'framer-motion'
-import useTextClipPath from '@/hooks/useTextClipPath'
-
 const vids = [
   { src: '/videos/randomVideos/eyes.mp4', alt: 'video 1' },
   { src: '/videos/randomVideos/feeling.mp4', alt: 'video 2' },
@@ -13,11 +8,7 @@ const vids = [
 export default function VideosSection() {
   return (
     <section className="relative w-full h-full py-12 px-1">
-      <div className="xl:sticky inset-0 max-w-xl text-8xl max-2xl:text-7xl max-xl:text-7xl font-extrabold tracking-[-2px] uppercase">
-        <motion.h6 {...useTextClipPath(0, true)}>~See &</motion.h6>
-        <motion.h6 {...useTextClipPath(0.1, true)}>~Feel &</motion.h6>
-        <motion.h6 {...useTextClipPath(0.2, true)}>~BeAware!</motion.h6>
-      </div>
+      <hr className="text-bg/50 mb-24" />
 
       {vids.map((vid, index) => (
         <div key={index} className="sticky top-0 right-0 flex flex-col items-end">
