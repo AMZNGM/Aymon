ac05febc69cf895195fa1d94efb3aefcd3e86bfd
NGM <ppmm91453@gmail.com>
2025-12-12 23:51:15 +0200

    add slogun

--- src/components/ui/Slogun.jsx
+++ src/components/ui/Slogun.jsx
@@ -1,12 +1,21 @@
+'use client'
+
+import { motion } from 'motion/react'
+
 export default function Slogun() {
   return (
-    <section className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-9xl font-sec">
+    <motion.section
+      initial={{ opacity: 0, filter: 'blur(10px)' }}
+      animate={{ opacity: 1, filter: 'blur(0px)' }}
+      transition={{ duration: 1.75 }}
+      className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-9xl font-sec"
+    >
       <span className="absolute top-4 left-4">i</span>
       <span className="absolute top-56 left-1/4">shut</span>
       <span className="absolute top-4 right-4">my</span>
       <span className="absolute bottom-8 left-4">Eyes</span>
       <span className="absolute bottom-8 left-1/2">to</span>
       <span className="absolute bottom-8 right-4">see</span>
-    </section>
+    </motion.section>
   )
 }
