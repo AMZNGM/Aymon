5600ecb3f2c25e318b6f4d9aa5a9c467156b1fe3
NGM <ppmm91453@gmail.com>
2025-12-14 10:06:33 +0200

    fix Slogun on mobile

--- src/components/ui/Slogun.jsx
+++ src/components/ui/Slogun.jsx
@@ -8,14 +8,14 @@ export default function Slogun() {
       initial={{ opacity: 0, filter: 'blur(10px)' }}
       animate={{ opacity: 1, filter: 'blur(0px)' }}
       transition={{ duration: 1.75 }}
-      className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-9xl font-sec"
+      className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-9xl max-md:text-6xl font-sec"
     >
-      <span className="absolute top-4 left-4">i</span>
+      <span className="absolute top-4 left-4 max-md:top-42">i</span>
       <span className="absolute top-56 left-1/4">shut</span>
-      <span className="absolute top-4 right-4">my</span>
-      <span className="absolute bottom-8 left-4">Eyes</span>
-      <span className="absolute bottom-8 left-1/2">to</span>
-      <span className="absolute bottom-8 right-4">see</span>
+      <span className="absolute top-4 right-4 max-md:top-40">my</span>
+      <span className="absolute bottom-8 left-4 max-md:bottom-72">Eyes</span>
+      <span className="absolute bottom-8 md:left-1/2 max-md:bottom-65 max-md:right-10">to</span>
+      <span className="absolute bottom-8 right-4 max-md:right-1/2">see</span>
     </motion.section>
   )
 }
