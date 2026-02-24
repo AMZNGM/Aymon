8c78f2b5c2511bcbcaa6799b143f919c43db0085
NGM <ppmm91453@gmail.com>
2026-02-24 15:06:38 +0200

    change the marqee path

--- src/components/home-components/LogosMarquee.jsx
+++ src/components/home-components/LogosMarquee.jsx
@@ -6,7 +6,7 @@ import ImageIn from '@/components/ui/unstyled/ImageIn'
 import ImagesMarquee from '@/components/ui/ImagesMarquee'
 
 const path =
-  'M51.5 517.283C99.5365 470.388 121.805 401.348 164.65 349.482C221.314 280.89 292.786 228.745 360.439 167.781C434.73 100.835 495.973 42.7027 618.524 52.6037C697.315 58.9693 788.979 132.091 849.91 167.781C905.115 200.116 972.11 224.78 1024.09 259.128C1118.76 321.694 1127.34 483.921 1240.41 517.283C1367.43 554.758 1492.5 548.155 1623.56 570.9C1670.95 579.124 2060.5 435.499 2060.5 435.499C2060.5 435.499 2109 408.409 2198.5 393.499C2320.77 373.131 2440 349.482 2564 349.482C2679 349.482 2644.59 349.482 2755.5 349.482'
+  'M51.5 517.243C211.013 361.522 344.994 30.4648 618.524 52.5637C922.173 77.0959 1020.81 452.453 1240.41 517.243C1722.64 659.519 2250.74 349.442 2755.5 349.442'
 
 export default function LogosMarquee() {
   const [logos, setLogos] = useState([])
@@ -22,7 +22,7 @@ export default function LogosMarquee() {
   if (!logos || logos.length === 0) return null
 
   return (
-    <section className="relative w-full h-fit bg-text text-bg pt-12">
+    <section className="relative w-full h-fit max-md:-translate-x-28 max-md:-translate-y-12 pt-6 max-md:pt-22">
       <ImagesMarquee path={path} viewBox="50 50 2000 600" spacing={1.4}>
         {logos.map((logo, index) => (
           <div key={logo.firestoreId || index} className="group w-24 hover:scale-125 transition-transform duration-300 ease-in-out">
