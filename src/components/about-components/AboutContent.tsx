7df32e68aa5670932be0c5c1c1ef65ac8271b1f4
NGM <ppmm91453@gmail.com>
2026-04-05 06:41:07 +0200

    neew mobile design for the about page

--- src/components/about-components/AboutContent.tsx
+++ src/components/about-components/AboutContent.tsx
@@ -6,17 +6,12 @@ import { motion } from 'framer-motion'
 import { gsap } from '@/utils/gsapConfig'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { getAboutContent } from '@/lib/getAbout'
-import { useIsMobile } from '@/hooks/useIsMobile'
 import AnimIn from '@/components/ui/unstyled/AnimIn'
 import AnimText from '@/components/ui/unstyled/AnimText'
 import LoadingOverlay from '@/components/shared/LoadingOverlay'
 import type { AboutContent } from '@/types/admin.types'
 
 const IMAGES = [
-  // '/images/2dModal/11.png',
-  // '/images/2dModal/12.png',
-  // '/images/2dModal/13.png',
-  // '/images/2dModal/14.png',
   '/images/2dModal/20.png',
   '/images/2dModal/21.png',
   '/images/2dModal/22.png',
@@ -25,20 +20,6 @@ const IMAGES = [
   '/images/2dModal/25.png',
   '/images/2dModal/26.png',
   '/images/2dModal/27.png',
-]
-
-const RANDOM_IMAGES = [
-  '/images/hero-Images/Asfour.webp',
-  '/images/hero-Images/caligula.webp',
-  '/images/hero-Images/Crow2.webp',
-  '/images/hero-Images/Folk.webp',
-  '/images/hero-Images/Forcing.webp',
-  '/images/hero-Images/Inside.webp',
-  '/images/hero-Images/Metro.webp',
-  '/images/hero-Images/Working24.webp',
-  '/images/hero-Images/Perspective.webp',
-  '/images/hero-Images/Pigeon.webp',
-  '/images/hero-Images/Proof.webp',
 ]
 
 function BioText({ text, className }: { text: string; className?: string }) {
@@ -69,30 +50,39 @@ function BioText({ text, className }: { text: string; className?: string }) {
 }
 
 export default function AboutContent() {
-  const isMobile = useIsMobile()
   const sectionRef = useRef<HTMLDivElement>(null)
   const [aboutContent, setAboutContent] = useState<AboutContent | null>(null)
   const [loading, setLoading] = useState(true)
   const [currentImage, setCurrentImage] = useState(0)
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
-  const [isHoveringBio, setIsHoveringBio] = useState(false)
-  const [randomImageIndex, setRandomImageIndex] = useState(0)
 
+  // Auto-cycle images on mobile
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % IMAGES.length)
+    }, 3000)
+    return () => clearInterval(interval)
+  }, [])
+
+  // scroll-driven image animation on desktop
   useEffect(() => {
     if (!sectionRef.current || loading) return
 
     const ctx = gsap.context(() => {
-      ScrollTrigger.create({
-        trigger: sectionRef.current,
-        start: 'top top',
-        end: `+=${IMAGES.length * 20}%`,
-        pin: true,
-        scrub: 1,
-        onUpdate: (self) => {
-          const progress = self.progress
-          const newIndex = Math.floor(progress * IMAGES.length)
-          const clampedIndex = Math.min(IMAGES.length - 1, newIndex)
-          setCurrentImage(clampedIndex)
+      ScrollTrigger.matchMedia({
+        '(min-width: 768px)': () => {
+          ScrollTrigger.create({
+            trigger: sectionRef.current,
+            start: 'top top',
+            end: `+=${IMAGES.length * 20}%`,
+            pin: true,
+            scrub: 1,
+            onUpdate: (self) => {
+              const progress = self.progress
+              const newIndex = Math.floor(progress * IMAGES.length)
+              const clampedIndex = Math.min(IMAGES.length - 1, newIndex)
+              setCurrentImage(clampedIndex)
+            },
+          })
         },
       })
     })
@@ -101,15 +91,7 @@ export default function AboutContent() {
     return () => ctx.revert()
   }, [loading])
 
-  const handleBioMouseMove = (e: React.MouseEvent) => {
-    const rect = e.currentTarget.getBoundingClientRect()
-    const y = e.clientY - rect.top
-    setMousePos({ x: e.clientX, y: e.clientY })
-    const percentage = y / rect.height
-    const newIndex = Math.floor(percentage * RANDOM_IMAGES.length)
-    setRandomImageIndex(Math.max(0, Math.min(RANDOM_IMAGES.length - 1, newIndex)))
-  }
-
+  // fetching
   useEffect(() => {
     async function fetchContent() {
       try {
@@ -130,76 +112,58 @@ export default function AboutContent() {
   }
 
   return (
-    <section
-      ref={sectionRef}
-      className="relative flex justify-between gap-4 w-dvw min-h-dvh uppercase px-4 max-md:px-1 md:pe-18 max-md:pt-18"
-    >
+    <section ref={sectionRef} className="relative flex justify-between gap-4 w-dvw min-h-dvh uppercase px-4 md:pe-18">
       {/* Left Side */}
-      <div className="z-10 flex flex-col justify-between gap-12 max-md:text-text md:p-8 max-md:mix-blend-difference">
-        <AnimText as="h2" delay={0.3} className="font-extrabold text-[7.2dvw] leading-[7dvw]">
-          {aboutContent.title}
-        </AnimText>
+      <div className="flex flex-col justify-between gap-12 mt-18">
+        <div className="flex items-center">
+          <AnimText as="h2" delay={0.3} className="font-extrabold text-[7.2dvw] leading-[7dvw]">
+            {aboutContent.title}
+          </AnimText>
 
-        <AnimText as="h2" delay={0.3} className="opacity-60 font-bold text-2xl tracking-tighter">
-          Biography
-        </AnimText>
-
-        <div
-          onMouseMove={handleBioMouseMove}
-          onMouseEnter={() => setIsHoveringBio(true)}
-          onMouseLeave={() => setIsHoveringBio(false)}
-          className="relative flex-1 h-full"
-        >
-          <AnimIn center blur delay={0.2} className="flex flex-col md:justify-evenly gap-8 md:gap-18 h-full pe-4">
-            <BioText text={aboutContent.bio1 || ''} className="font-medium max-md:text-sm text-2xl leading-relaxed" />
-            <BioText text={aboutContent.bio2 || ''} className="font-medium max-md:text-sm text-2xl leading-relaxed" />
-            <BioText text={aboutContent.bio3 || ''} className="font-medium max-md:text-sm text-2xl leading-relaxed" />
-          </AnimIn>
-
-          {/* Mouse Follower IMAGES */}
-          {isHoveringBio && !isMobile && (
-            <motion.div
-              style={{ left: mousePos.x + 20, top: mousePos.y - 100 }}
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.8 }}
-              transition={{ duration: 0.2 }}
-              className="z-50 fixed w-75 h-75 overflow-hidden shadow-2xl rounded-lg pointer-events-none"
-            >
-              {RANDOM_IMAGES.map((src, index) => (
-                <Image
-                  key={src}
-                  src={src}
-                  alt={`Random image ${index + 1}`}
-                  fill
-                  sizes="300px"
-                  style={{ opacity: randomImageIndex === index ? 1 : 0 }}
-                  className="object-cover"
-                />
-              ))}
-            </motion.div>
-          )}
+          <div className="w-8 md:w-22 h-px md:h-1 bg-bg ms-12" />
         </div>
 
-        <AnimIn center blur delay={0.3} className="max-md:mb-8">
-          <p className="flex gap-6 opacity-60 font-sec text-lg">
-            <span>I Shut My</span>
-            <span className="relative">
-              **
-              <span className="top-1.5 left-1/2 absolute rotate-90 -translate-x-1/2">)</span>
-            </span>
-            <span>eyes to See</span>
-          </p>
+        <div className="flex justify-between items-start">
+          <AnimText as="h2" delay={0.3} className="opacity-60 font-bold md:text-2xl tracking-tighter">
+            Biography
+          </AnimText>
+
+          <span className="md:hidden opacity-60 font-bold leading-tight tracking-tighter">
+            est 2003 <br /> Known as - {aboutContent.nickname}
+          </span>
+        </div>
+
+        <div className="md:hidden relative w-full aspect-4/5">
+          <Image src={IMAGES[currentImage]} alt={`About image ${currentImage + 1}`} priority fill className="object-center object-cover" />
+        </div>
+
+        <p className="md:hidden opacity-60 font-bold text-center tracking-tighter">
+          [{aboutContent.position || 'Multidisciplinary Visual Artist'}]
+        </p>
+
+        <AnimIn center blur delay={0.2} className="relative flex flex-col flex-1 md:justify-evenly gap-10 md:gap-18 h-full md:pe-4">
+          <BioText text={aboutContent.bio1 || ''} className="font-medium md:text-2xl leading-relaxed max-md:leading-loose" />
+          <BioText text={aboutContent.bio2 || ''} className="font-medium md:text-2xl leading-relaxed max-md:leading-loose" />
+          <BioText text={aboutContent.bio3 || ''} className="font-medium md:text-2xl leading-relaxed max-md:leading-loose" />
+        </AnimIn>
+
+        <AnimIn as="p" center blur delay={0.3} className="flex max-md:justify-center gap-6 opacity-60! font-sec text-xl mb-12">
+          <span>I Shut My</span>
+          <span className="relative">
+            **
+            <span className="top-1.5 left-1/2 absolute rotate-90 -translate-x-1/2">)</span>
+          </span>
+          <span>eyes to See</span>
         </AnimIn>
       </div>
 
       {/* Right Side */}
-      <div className="-z-10 md:z-0 max-md:fixed max-md:inset-0 flex flex-col justify-between gap-12 w-full bg-text md:p-8">
-        <span className="max-md:hidden opacity-60 font-bold text-sm md:text-2xl leading-tight tracking-tighter">
+      <div className="max-md:hidden flex flex-col w-full p-8">
+        <span className="opacity-60 font-bold text-2xl leading-tight tracking-tighter">
           est 2003 <br /> Known as - {aboutContent.nickname}
         </span>
 
-        <div className="relative flex-1 max-md:scale-70 max-md:origin-top max-md:mt-18">
+        <div className="relative w-full min-h-0 aspect-4/5 grow">
           {IMAGES.map((src, index) => (
             <motion.div
               key={src}
@@ -208,12 +172,12 @@ export default function AboutContent() {
               transition={{ duration: 0, ease: 'easeOut' }}
               className="absolute inset-0"
             >
-              <Image src={src} alt={`About image ${index + 1}`} priority={index === 0} fill className="object-cover md:object-contain" />
+              <Image src={src} alt={`About image ${index + 1}`} priority={index === 0} fill className="object-center object-contain" />
             </motion.div>
           ))}
         </div>
 
-        <p className="max-md:hidden opacity-60 font-bold text-[1.4dvw] max-md:text-sm tracking-tighter">
+        <p className="opacity-60 font-bold text-[1.4dvw] text-nowrap tracking-tighter">
           [{aboutContent.position || 'Multidisciplinary Visual Artist'}]
         </p>
       </div>
